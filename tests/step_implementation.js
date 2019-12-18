3fe7625272d7f8d3de728253426bef88c2bd99d9
amoghdhumal <amoghdhumal@gmail.com>
2019-12-18 12:53:50 +0530

    vowel

--- tests/step_implementation.js
+++ tests/step_implementation.js
@@ -1,31 +1,45 @@
 /* globals gauge*/
+
 "use strict";
-const { openBrowser,write, closeBrowser, goto, press, text, focus, textBox, toRightOf } = require('taiko');
-const assert = require("assert");
-const headless = process.env.headless_chrome.toLowerCase() === 'true';
 
-beforeSuite(async () => {
-    await openBrowser({ headless: headless })
+/**
+ * Loads the `assert` module provided by NodeJS
+ */
+var assert = require("assert");
+
+
+/**
+ * Loads the local `vowels.js` module present in this directory
+ */
+var vowels = require("./vowels");
+
+
+// --------------------------
+// Gauge step implementations
+// --------------------------
+
+gauge.step("Vowels in English language are <vowels>.", function(vowelsGiven) {
+  assert.equal(vowelsGiven, vowels.vowelList.join(""));
 });
 
-afterSuite(async () => {
-    await closeBrowser();
+gauge.step("The word <word> has <number> vowels.", function(word, number) {
+  assert.equal(number, vowels.numVowels(word));
 });
 
-step("Goto getgauge github page", async () => {
-    await goto('https://github.com/getgauge');
+gauge.step("Almost all words have vowels <table>", function(table) {
+  table.rows.forEach(function (row) {
+    assert.equal(vowels.numVowels(row.cells[0]), parseInt(row.cells[1]));
+  });
 });
 
-step("Search for <query>", async (query) => {
-    await focus(textBox(toRightOf('Pricing')))
-    await write(query);
-    await press('Enter');
+// ---------------
+// Execution Hooks
+// ---------------
+
+gauge.hooks.beforeScenario(function () {
+  assert.equal(vowels.vowelList.join(""), "aeiou");
 });
 
-step("Page contains <content>", async (content) => {
-    assert.ok(await text(content).exists());
-});
-
-step("Page Contains", async()=> {
-assert.ok(await text("gauge-java").exists)
-});
+gauge.hooks.beforeScenario(function () {
+  assert.equal(vowels.vowelList[0], "a");
+}, { tags: [ "single word" ]});
